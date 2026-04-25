042c3efe3e870a6108a31a1b7e7dcb15e462c589
konnyeden <125564555+konnyeden@users.noreply.github.com>
2026-04-25 12:30:08 +0300

    Add files via upload

--- init_db.py
+++ init_db.py
@@ -1,12 +1,7 @@
-from .database import engine
-from .models import Base
-
-
+from database import engine
+from models import Base
 def main():
-    # Создать все таблицы в БД, если их еще нет
     Base.metadata.create_all(bind=engine)
     print("Database tables created (if not exist).")
-
-
-if __name__ == "__main__":
+if __name__ == "main":
     main()
